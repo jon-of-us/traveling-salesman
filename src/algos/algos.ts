f0aa9504a207d21e7883284b98f2f11b6be3608a
Phytoplancton <jonas-ullmann@web.de>
2023-10-06 19:11:44 +0200

    add run function

--- src/algos/algos.ts
+++ src/algos/algos.ts
@@ -1,7 +1,13 @@
 import { Nodes } from "../data/nodes";
 import { Edges } from "../data/edges";
+import { algoFunction, type algoOptions } from "./algotypes";
 
 export type algorithm = (nodes: Nodes, edges: Edges) => void;
 
 export const random: algorithm = (nodes, edges) => {};
 export const other: algorithm = (nodes, edges) => {};
+
+export function run(nodes: Nodes, edges: Edges, algoType: algoOptions): void {
+    const algoFunc = algoFunction[algoType];
+    algoFunc(nodes, edges);
+}
