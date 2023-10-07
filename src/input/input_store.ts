3be2f070d93bfbdc94eb20a1215860104fbbca5b
Phytoplancton <jonas-ullmann@web.de>
2023-10-08 00:25:20 +0200

    changed number of points

--- src/input/input_store.ts
+++ src/input/input_store.ts
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 import { algoLabels } from "../algos/algo_utils";
 
 export const input_store = writable({
-    nPoints: 3,
+    nPoints: 20,
     starterAlgo: algoLabels.random,
     optimizerAlgo: algoLabels.twoOpt,
 });
